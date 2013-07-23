ad92d2a18b5be35f9dbb203755a508757507be2c
MA7 <amini141@gmail.com>
2013-07-23 11:04:41 +0430

    update

--- verify.php
+++ verify.php
@@ -4,20 +4,21 @@
 	$Amount = 1000; //Amount will be based on Toman
 	$Authority = $_GET['Authority'];
 	
-	
 	if($_GET['Status'] == 'OK'){
-			$client = new SoapClient('http://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); // URL also Can be https://de.zarinpal.com/pg/services/WebGate/wsdl
-			$result = $client->PaymentVerification(
-				array(
-						'MerchantID'	 => $MerchantID,
-						'Authority' 	 => $Authority,
-						'Amount'	 	 => $Amount
-					)
-			);
+		// URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
+		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
+		
+		$result = $client->PaymentVerification(
+												array(
+														'MerchantID'	 => $MerchantID,
+														'Authority' 	 => $Authority,
+														'Amount'	 	 => $Amount
+													)
+		);
 
-		echo  $result->Status;  // Status of payment
-		echo $transid = $result->RefID; // RefID of Payment 
-	}else{
+		echo $result->Status;  // Status of payment
+		echo $result->RefID; // RefID of Payment 
+	} else {
 		echo 'Transaction canceled by user';
 	}
 	
