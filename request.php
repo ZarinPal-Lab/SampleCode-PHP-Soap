3ba8454385fe86e961f1a286be6b22906f4915d7
Mostafa Amiri <amiri27@gmail.com>
2016-05-27 16:32:02 +0430

    Small code styling fix

--- request.php
+++ request.php
@@ -10,20 +10,18 @@
     // URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
     $client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', ['encoding' => 'UTF-8']);
 
-    $result = $client->PaymentRequest(
-                        [
-                                'MerchantID'     => $MerchantID,
-                                'Amount'         => $Amount,
-                                'Description'    => $Description,
-                                'Email'          => $Email,
-                                'Mobile'         => $Mobile,
-                                'CallbackURL'    => $CallbackURL,
-                            ]
-    );
+    $result = $client->PaymentRequest([
+        'MerchantID'     => $MerchantID,
+        'Amount'         => $Amount,
+        'Description'    => $Description,
+        'Email'          => $Email,
+        'Mobile'         => $Mobile,
+        'CallbackURL'    => $CallbackURL
+    ]);
 
     //Redirect to URL You can do it also by creating a form
     if ($result->Status == 100) {
-        Header('Location: https://www.zarinpal.com/pg/StartPay/'.$result->Authority);
+        header('Location: https://www.zarinpal.com/pg/StartPay/'.$result->Authority);
     } else {
         echo'ERR: '.$result->Status;
     }
